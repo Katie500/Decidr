48a64dc94ab4063260ee19d3341c188a736640eb
Arya <aryaprabhu290399@gmail.com>
2023-11-25 20:18:45 -0700

    adding start new session view

--- client/src/components/Views/startNewSession.js
+++ client/src/components/Views/startNewSession.js
@@ -68,11 +68,9 @@ function StartNewSession() {
                 <Form.Select>
                   <option>1</option>
                   <option>2</option>
-
                   <option>3</option>
                   <option>4</option>
                   <option>5</option>
-
                   <option>6</option>
                   <option>7</option>
                 </Form.Select>
