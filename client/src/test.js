03e382f09f46a6b19d41f9bd483854ef682f15f4
Arya <aryaprabhu290399@gmail.com>
2023-11-25 19:05:53 -0700

    adding default and nickname views

--- client/src/test.js
+++ client/src/test.js
@@ -1,11 +1,15 @@
 import Timer from "./components/UI/timer";
+import Default from "./components/Views/default";
+import Nickname from "./components/Views/nickname";
 import Results from "./components/Views/results";
 import View2 from "./components/Views/view2";
 
 const Test = () => (
   <div>
     {/* <View2 /> */}
-    <Results />
+    {/* <Results /> */}
+    {/* <Nickname /> */}
+    <Default />
   </div>
 );
 
