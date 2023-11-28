28b730871e8216c3258ce966d15b1f868a09c442
Arya <aryaprabhu290399@gmail.com>
2023-11-28 11:42:48 -0700

    moving aaron's code to components folder

--- client/src/test.js
+++ client/src/test.js
@@ -8,10 +8,10 @@ import View2 from "./components/Views/view2";
 
 const Test = () => (
   <div>
-    <View2 />
+    {/* <View2 /> */}
     {/* <Results /> */}
     {/* <Nickname /> */}
-    {/* <Default /> */}
+    <Default />
     {/* <StartNewSession /> */}
   </div>
 );
