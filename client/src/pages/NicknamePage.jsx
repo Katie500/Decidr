ac829c07e4069f2c2aab5703294112fbd21daf50
luqmanuofc <luqman.bukhari@ucalgary.ca>
2023-11-30 13:06:56 -0700

    added nickname generation

--- client/src/pages/NicknamePage.jsx
+++ client/src/pages/NicknamePage.jsx
@@ -1,18 +1,31 @@
-import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React, { useState } from 'react';
 import LoadingBackdrop from '../components/global/LoadingBackdrop';
+import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from 'unique-names-generator';
 
 const MainPage = ({ sessionCode }) => {
   const [pending, setPending] = useState(false);
   const [name, setName] = useState('');
 
-  // A USE E
-
-  const handleVerify = () => {
-    setPending(true);
-    setTimeout(() => {
-      setPending(false);
-    }, 2000);
+  const handleAnonymousName = () => {
+    const shortName = uniqueNamesGenerator({
+      dictionaries: [adjectives, animals, colors],
+      length: 2,
+    }); // big-donkey
+    setName(shortName);
   };
 
   const handleStart = () => {
@@ -23,39 +36,47 @@ const MainPage = ({ sessionCode }) => {
   };
 
   return (
-    <Grid className="container">
-      <Box>
-        <Typography
-          variant="h4"
-          className="title"
-          marginTop={2}
-          textAlign={'center'}
-        >
-          Decidr
-        </Typography>
+    <>
+      <Box className="topBarContainer">
+        <Box container className="topBar widthConstraint">
+          <IconButton className="topBarIcon">
+            <ArrowBackOutlinedIcon />
+          </IconButton>
+          <Typography variant="h6">Decidr</Typography>
+        </Box>
       </Box>
-      <Box className="contentBox widthConstraint">
-        <Typography variant="h6">Enter a nickname:</Typography>
-        <Box className="inputBox">
-          <TextField fullWidth label="Name" variant="outlined" size="small" />
-          <Button variant="contained" onClick={handleVerify}>
-            Go
+
+      <Grid className="container">
+        <Box className="contentBox widthConstraint">
+          <Typography variant="h6">Enter a nickname:</Typography>
+          <Box className="inputBox">
+            <TextField
+              fullWidth
+              label="Name"
+              variant="outlined"
+              size="small"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Button variant="contained" onClick={handleStart}>
+              Go
+            </Button>
+          </Box>
+          <Typography variant="h6" margin={1} textAlign={'center'}>
+            or
+          </Typography>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleAnonymousName}
+            fullWidth
+          >
+            Give me an anonymous name
           </Button>
         </Box>
-        <Typography variant="h6" margin={1} textAlign={'center'}>
-          or
-        </Typography>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={handleStart}
-          fullWidth
-        >
-          Give me an anonymous name
-        </Button>
-      </Box>
-      <LoadingBackdrop open={pending} />
-    </Grid>
+        <LoadingBackdrop open={pending} />
+      </Grid>
+    </>
   );
 };
 
