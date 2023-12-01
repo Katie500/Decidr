7622fb1290234667f3031593f5bcefde65e0b6f5
samir.gulahmadov1 <samir.gulahmadov1@cs.ucalgary.ca>
2023-11-30 20:26:23 -0600

    Changed the schemas

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,3 @@
-// Instead of using export default, use module.exports
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -6,6 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: { type: String, required: true },
+    roomID: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
