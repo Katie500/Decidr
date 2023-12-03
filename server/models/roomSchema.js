5f24f6471959fb31852ef6d5296548c3f1d88ebb
luqmanuofc <luqman.bukhari@ucalgary.ca>
2023-12-02 21:42:40 -0700

    updated schemas

--- server/models/roomSchema.js
+++ server/models/roomSchema.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const voteOptionSchema = new Schema({
-    option: { type: String, required: true },
-    userIDs: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  option: { type: String, required: true },
+  userIDs: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const roomSchema = new Schema({
-    RoomID: { type: String, required: true, unique: true },
-    question: { type: String, required: true },
-    voteOptions: [voteOptionSchema],
-    endTime: { type: Date, required: true },
-    ownerUserID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  roomID: { type: String, required: true },
+  question: { type: String, required: true },
+  voteOptions: [voteOptionSchema],
+  endTime: { type: Date, required: true },
+  ownerUserID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 const Room = mongoose.model('Room', roomSchema);
--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -2,34 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://0.0.0.0:27017/users")
-.then(() => {
-    console.log('mongodb connected');
-})
-.catch(() => {
-    console.log('error');
-})
-
-
-
 const userSchema = new Schema({
-
-    roomID: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true },
-    username: { type: String, required: true },
+  roomID: { type: String, required: false },
+  socketID: { type: String, required: true },
+  username: { type: String, required: true },
 });
 
 const User = new mongoose.model('User', userSchema);
 
-// Import data to the user in MongoDB database
-/*
-const importUserData = {
-    userID: "1",
-    roomID: "Room1",
-    isAdmin: true,
-    username: "Aaron",
-  };
-
-User.insertMany([importUserData]);
-*/
 module.exports = User;
