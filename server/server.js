f21f2a4cb73a253a56499520c9b6a64cb1829749
luqmanuofc <luqman.bukhari@ucalgary.ca>
2023-12-02 21:46:04 -0700

    added roomSchema to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const cors = require("cors");
+const http = require('http');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const PORT = 3001;
 
 app.use(cors());
@@ -12,56 +12,58 @@ app.use(express.json());
 const server = http.createServer(app);
 
 // Database Schemas
-const User = require("./models/userSchema");
-const Room = require("./models/roomSchema");
+const User = require('./models/userSchema');
+const Room = require('./models/roomSchema');
 
 // Require the Routes
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require('./routes/apiRoutes');
+const createUserAndRoom = require('./routes/createUserAndRoom');
 
 // Use the apiRoutes
 app.use('/', apiRoutes);
+app.use('/createUserAndRoom', createUserAndRoom);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 
-
 //==================================== ROOM CONNECTION =========================//
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
+  socket.on('join_room', (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
-  socket.on("check_room", (data, callback) => {
+  socket.on('check_room', (data, callback) => {
     const roomExists = io.sockets.adapter.rooms.has(data);
     callback(roomExists);
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
   });
 });
-
 
 //============================ SERVER RESPONSE =============================//
 
 // Connect to MongoDB using mongoose
-mongoose.connect("mongodb://0.0.0.0:27017/users")
+mongoose
+  .connect('mongodb://0.0.0.0:27017/decidr')
   .then(() => {
     console.log('MongoDB connected');
 
     // Run the userSchema.js code
-    require("./models/userSchema");
+    require('./models/userSchema');
+    require('./models/roomSchema');
 
     // Start the server after MongoDB connection is established
     server.listen(PORT, () => {
