dcc725fbb9c9ba7f0723da1bb86d7759b224aff0
Arya <aryaprabhu290399@gmail.com>
2023-12-06 14:00:10 -0700

    adding label truncation

--- client/src/pages/Room/BubbleChart.jsx
+++ client/src/pages/Room/BubbleChart.jsx
@@ -2,29 +2,34 @@ import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const getRandomColor = () => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
 
-    // Construct the RGB color string
-    return "rgb(" + red + "," + green + "," + blue + ")";
+  // Construct the RGB color string
+  return "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 const getPercentage = (total, value) => {
   console.log(value + "/" + total);
-  return (value/total)*100;
-}
-
+  return (value / total) * 100;
+};
 
 const colorScale = (percent) => {
-  const color = d3.interpolate("lightgreen", "darkgreen")(percent/100);
+  const color = d3.interpolate("lightgreen", "darkgreen")(percent / 100);
   return color;
+};
+
+function truncateText(text, width) {
+  const ellipsis = "...";
+  const truncatedText =
+    text.length > width
+      ? text.slice(0, width - ellipsis.length) + ellipsis
+      : text;
+  return truncatedText;
 }
 
-const BubbleChart = ({
-  votionOptions,
-  totalAvailableVotes,
-}) => {
+const BubbleChart = ({ votionOptions, totalAvailableVotes }) => {
   const chartRef = useRef(null);
 
   const growthFactor = 2;
@@ -44,17 +49,17 @@ const BubbleChart = ({
 
     //arranging data into a format that we can use with d3
     let data = [];
-    for (let i=0; i<votionOptions.length; i++){
+    for (let i = 0; i < votionOptions.length; i++) {
       const numOfVotes = votionOptions[i].votes.length;
-        const item = {
-          r: getPercentage(totalAvailableVotes, numOfVotes) * growthFactor, name: votionOptions[i].optionText, numOfVotes: numOfVotes
-        };
-        numOfVotes > 0 ? data.push(item) : data.push();
+      const item = {
+        r: getPercentage(totalAvailableVotes, numOfVotes) * growthFactor,
+        name: votionOptions[i].optionText,
+        numOfVotes: numOfVotes,
+      };
+      numOfVotes > 0 ? data.push(item) : data.push();
     }
 
     // const colorScale = d3.scaleLinear().domain([0, 1]).range(['green', 'red']);
-
-   
 
     // Drawing bubbles
     const simulation = d3
@@ -63,20 +68,8 @@ const BubbleChart = ({
         "collide",
         d3.forceCollide().radius((d) => d.r + 2)
       ) // Simulation will force bubbles to be as close as possible
-      .force(
-        "x",
-        d3
-          .forceX()
-          .strength(speed)
-          .x(center_x)
-      ) // Horizontal location
-      .force(
-        "y",
-        d3
-          .forceY()
-          .strength(speed)
-          .y(center_y)
-      ); // Vertical location
+      .force("x", d3.forceX().strength(speed).x(center_x)) // Horizontal location
+      .force("y", d3.forceY().strength(speed).y(center_y)); // Vertical location
 
     // Create bubbles for each data point with random positions and style them
     svg
@@ -87,9 +80,8 @@ const BubbleChart = ({
       .attr("r", (d) => d.r)
       .style("fill", () => getRandomColor())
       // .style("fill", (d) => colorScale(d.r/growthFactor))
-      .style('stroke', 'black') // Border color
-      .style('stroke-width', 2) // Border width
-      
+      .style("stroke", "black") // Border color
+      .style("stroke-width", 2); // Border width
 
     // Add text labels for each bubble
     svg
@@ -98,10 +90,10 @@ const BubbleChart = ({
       .enter()
       .append("text")
       .attr("text-anchor", "middle")
-      .attr("dy", (d) => d.r/8)
+      .attr("dy", (d) => d.r / 8)
       .style("fill", "white")
       .style("font-size", "18px")
-      .text((d) => d.name);
+      .text((d) => truncateText(d.name, d.r / 6));
 
     svg
       .selectAll("numOfVotes")
@@ -109,10 +101,11 @@ const BubbleChart = ({
       .enter()
       .append("text")
       .attr("text-anchor", "middle")
-      .attr("dy", (d) => d.r/8 + 18)
+      .attr("dy", (d) => d.r / 8 + 18)
       .style("fill", "white")
       .style("font-size", "18px")
-      .text((d) => d.numOfVotes);  
+      .style("width", (d) => d.r)
+      .text((d) => d.numOfVotes);
 
     // Update positions in each tick of the simulation
     simulation.on("tick", () => {
@@ -133,9 +126,7 @@ const BubbleChart = ({
     });
   }, []);
 
-  return (
-    <svg ref={chartRef} width="100%" height="100%"/>
-  );
+  return <svg ref={chartRef} width="100%" height="100%" />;
 };
 
 export default BubbleChart;
