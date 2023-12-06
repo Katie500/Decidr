5e47062cdd257744fb866ad70e0c31e8dd7d6934
Arya <aryaprabhu290399@gmail.com>
2023-12-05 22:59:56 -0700

    cleaning up bubbles

--- client/src/pages/Room/BubbleChart.jsx
+++ client/src/pages/Room/BubbleChart.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const getRandomColor = () => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    // Construct the RGB color string
+    return "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 const getPercentage = (total, value) => {
@@ -23,62 +23,62 @@ const colorScale = (percent) => {
 
 const BubbleChart = ({
   votionOptions,
-  handleAddVote,
-  handleRemoveVote,
-  handleAddOption,
-  userID,
   totalAvailableVotes,
 }) => {
   const chartRef = useRef(null);
 
-  const growthFactor = 2.5;
+  const growthFactor = 2;
+  const speed = 0.05;
+  const center_x = 200;
+  const center_y = 300;
 
-  console.log("votionOptions",votionOptions);
-  console.log("Number of votes",votionOptions[0].votes.length);
+  // console.log("votionOptions",votionOptions);
+  // console.log("Number of votes",votionOptions[0].votes.length);
   // console.log("userID",userID);
-  console.log("totalAvailableVotes",totalAvailableVotes);
+  // console.log("totalAvailableVotes",totalAvailableVotes);
 
-  console.log(getPercentage(totalAvailableVotes, votionOptions[0].votes.length));
+  // console.log(getPercentage(totalAvailableVotes, votionOptions[0].votes.length));
 
   useEffect(() => {
     const svg = d3.select(chartRef.current);
 
+    //arranging data into a format that we can use with d3
     let data = [];
     for (let i=0; i<votionOptions.length; i++){
+      const numOfVotes = votionOptions[i].votes.length;
         const item = {
-          r: getPercentage(totalAvailableVotes, votionOptions[i].votes.length) * growthFactor, name: votionOptions[i].optionText
+          r: getPercentage(totalAvailableVotes, numOfVotes) * growthFactor, name: votionOptions[i].optionText, numOfVotes: numOfVotes
         };
-        votionOptions[i].votes.length > 0 ? data.push(item) : data.push();
+        numOfVotes > 0 ? data.push(item) : data.push();
     }
 
     // const colorScale = d3.scaleLinear().domain([0, 1]).range(['green', 'red']);
 
    
 
-    // Create a force simulation
+    // Drawing bubbles
     const simulation = d3
       .forceSimulation(data)
       .force(
         "collide",
-        //dividing radius by growth factor because we multiply it above
         d3.forceCollide().radius((d) => d.r + 2)
-      ) // Adjust the padding as needed
+      ) // Simulation will force bubbles to be as close as possible
       .force(
         "x",
         d3
           .forceX()
-          .strength(0.1)
-          .x(400 / 2)
-      ) // Center on the x-axis
+          .strength(speed)
+          .x(center_x)
+      ) // Horizontal location
       .force(
         "y",
         d3
           .forceY()
-          .strength(0.1)
-          .y(600 / 2)
-      ); // Center on the y-axis
+          .strength(speed)
+          .y(center_y)
+      ); // Vertical location
 
-    // Create circles for each data point with random positions
+    // Create bubbles for each data point with random positions and style them
     svg
       .selectAll("circle")
       .data(data)
@@ -86,28 +86,33 @@ const BubbleChart = ({
       .append("circle")
       .attr("r", (d) => d.r)
       .style("fill", () => getRandomColor())
-      .style("fill", (d) => colorScale(d.r/growthFactor))
+      // .style("fill", (d) => colorScale(d.r/growthFactor))
       .style('stroke', 'black') // Border color
       .style('stroke-width', 2) // Border width
-      .call(
-        d3
-          .drag() // Enable dragging for circles
-          .on("start", dragStarted)
-          .on("drag", dragged)
-          .on("end", dragEnded)
-      );
+      
 
-    // Add text labels for each circle
+    // Add text labels for each bubble
     svg
       .selectAll("text")
       .data(data)
       .enter()
       .append("text")
       .attr("text-anchor", "middle")
-      .attr("dy", "0.4em") // Adjust the vertical alignment
+      .attr("dy", (d) => d.r/8)
       .style("fill", "white")
-      .style("font-size", "12px")
+      .style("font-size", "18px")
       .text((d) => d.name);
+
+    svg
+      .selectAll("numOfVotes")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("dy", (d) => d.r/8 + 18)
+      .style("fill", "white")
+      .style("font-size", "18px")
+      .text((d) => d.numOfVotes);  
 
     // Update positions in each tick of the simulation
     simulation.on("tick", () => {
@@ -120,30 +125,16 @@ const BubbleChart = ({
         .selectAll("text")
         .attr("x", (d) => d.x)
         .attr("y", (d) => d.y);
+
+      svg
+        .selectAll("numOfVotes")
+        .attr("x", (d) => d.x)
+        .attr("y", (d) => d.y);
     });
-
-    function dragStarted(event, d) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(event, d) {
-      d.fx = event.x;
-      d.fy = event.y;
-    }
-
-    function dragEnded(event, d) {
-      if (!event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
   }, []);
 
   return (
-    <svg ref={chartRef} width="100%" height="100%" onClick={() => alert("hi")}>
-      {/* Any additional SVG elements can be added here */}
-    </svg>
+    <svg ref={chartRef} width="100%" height="100%"/>
   );
 };
 
