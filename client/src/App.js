9c3535cd013847dfefa3c57fdb0f4f4cd5b2fef2
luqmanuofc <luqman.bukhari@ucalgary.ca>
2023-12-06 18:55:04 -0700

    removed test files

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,6 @@ import Room from "./pages/Room/RoomPage";
 import ResultPage from "./pages/ResultPage";
 import { SocketProvider } from "./contexts/SocketContext";
 import { NotificationProvider } from "./contexts/NotificationContext";
-// TESTING:
-import TestUser from "./components/Test/apiTest";
-import TestRoom from "./components/Test/apiTest2";
-import TestAvatar from "./components/Test/AvatarPage";
-import TestPage1 from "./components/Test/page1";
-import TestPage2 from "./components/Test/page2";
 
 function App() {
   const theme = createTheme({
@@ -43,13 +37,6 @@ function App() {
                 <Route path="/startnewroom" element={<StartNewRoom />} />
                 <Route path="/room" element={<Room />} />
                 <Route path="/resultpage" element={<ResultPage />} />
-
-                {/* Testing code */}
-                <Route path="/testUser" element={<TestUser />} />
-                <Route path="/testRoom" element={<TestRoom />} />
-                <Route path="/testAvatar" element={<TestAvatar />} />
-                <Route path="/testPage1" element={<TestPage1 />} />
-                <Route path="/testPage2" element={<TestPage2 />} />
               </Routes>
             </BrowserRouter>
           </UserProvider>
--- client/src/components/UI/header.js
+++
@@ -1,9 +0,0 @@
-import { Row } from "react-bootstrap";
-
-const Header = () => (
-  <Row className="centered">
-    <h1>Decidr</h1>
-  </Row>
-);
-
-export default Header;
